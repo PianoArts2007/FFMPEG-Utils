b1a9d245f9166dc04ea0586b17b333520b02b779
PianoArts2007 <92180085+PianoArts2007@users.noreply.github.com>
2022-02-08 19:15:06 +0200

    Add files via upload

---
+++ FFMPEG Utils.cpp
@@ -0,0 +1,222 @@
+// FFMPEG Utils.cpp : Defines the entry point for the application.
+//
+//internal lib
+#include "framework.h"
+#include "FFMPEG Utils.h"
+//external lib
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <ddeml.h>
+using namespace std;
+
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+/////////
+
+using namespace std;
+
+
+int main() //ffmpeg data display
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_FFMPEGUTILS, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_FFMPEGUTILS));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_FFMPEGUTILS));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_FFMPEGUTILS);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT: //about box dialog | menu item: Help
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case ID_FILE_EXPORTVIDEO: //export video | menu item: File
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG6), hWnd, About);
+                break;
+            case ID_FILE_SETTINGS: //settings | menu item: File
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, About);
+                break;
+            case ID_Menu: //video compress size | menu item: Options
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG4), hWnd, About);
+                break;
+            case ID_OPTIONS_CONVERTVIDEOSFROMMKV: //converter | menu item: Options
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG3), hWnd, About);
+                break;
+            case ID_OPTIONS_SPEEDVIDEOUP: //speed up / down video speed | menu item: Options
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG5), hWnd, About);
+                break;
+            case ID_OPTIONS_ADDAUDIOTOVIDEO: //video + audio | menu item: Options
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG2), hWnd, About);
+                break;
+            case ID_HELP_REPORTBUGS: //Report bugs or request features | menu item: Help
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG7), hWnd, About);
+                break;
+            //case ID_FILE_IMPORTVIDEO:
+  
+            case IDM_EXIT: //program exit | menu item: File
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
---
+++ resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FFMPEG Utils.rc
+//
+#define IDC_MYICON                      2
+#define IDD_FFMPEGUTILS_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_FFMPEGUTILS                 107
+#define IDI_SMALL                       108
+#define IDC_FFMPEGUTILS                 109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDD_DIALOG2                     130
+#define IDD_DIALOG3                     131
+#define IDD_DIALOG4                     132
+#define IDD_DIALOG5                     133
+#define IDD_DIALOG6                     134
+#define IDD_DIALOG7                     135
+#define ID_FILE_IMPORTVIDEO             32771
+#define ID_FILE_EXPORTVIDEO             32772
+#define ID_FILE_SETTINGS                32773
+#define ID_Menu                         32774
+#define ID_HELP_OFFICIALSITE            32775
+#define ID_HELP_MANUAL                  32776
+#define ID_HELP_REPORTBUGS              32777
+#define ID_OPTIONS_CONVERTVIDEOSFROMMKV 32778
+#define ID_OPTIONS_SPEEDVIDEOUP         32779
+#define ID_OPTIONS_ADDAUDIOTOVIDEO      32780
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
